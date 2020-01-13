8214ee56f11e31da99139ed16db789f2780c4127
Jtaylorthegreat <46611199+Jtaylorthegreat@users.noreply.github.com>
2020-01-13 13:39:39 -0600

    added background color, brief appearance cleanup

--- video_index_page.php
+++ video_index_page.php
@@ -1,11 +1,13 @@
 <?php 
 	echo "<head><title>Video Indexed Page</title></head>";
-	echo "<body>";
+	echo "<body style='background-color:#000099'>";
+  	echo "<br>";
         $file_queue = "my_videos/";
         $video_files = scandir($file_queue);
         foreach($video_files as $file) {
               if (!in_array($file,array(".",".."))){
-                   echo "<pre><font color='black'> " . $file . "</font><br>";
+		   echo "<hr><br>";
+                   echo "<pre><font color='white'><b> " . $file . "</b></font><br>";
                    echo "<video width='600' controls>";
                    echo "<source src='" . $file_queue . $file . "' type='video/mp4'>";
                    echo "</video></pre>";
