1c7aef5a824af8baf8562e089e7f19bf6f9bddd7
Jtaylorthegreat <46611199+Jtaylorthegreat@users.noreply.github.com>
2020-01-20 14:15:16 -0600

    Update files.php

--- File_queue/files.php
+++ File_queue/files.php
@@ -23,43 +23,43 @@
     echo "<table class='a'><tr class='a'><th class='a'>File Name</th><th class='a'>Size</th><th class='a'>Date Created</th></tr>";
 
     foreach($served_files as $file) {
-   	if (!in_array($file,array(".",".."))) {
+   	if ((!in_array($file,array(".",".."))) && is_file($directory_served.$file)) {
         
-        $indexed_file_size = filesize($directory_served.$file);
-        $indexed_file_mod_date = date ("Y-d-m H:i:s", filemtime($directory_served.$file));
-        
-        echo "<tr class='a'>";
-        if ( $indexed_file_size >= GB){
-          $indexed_file_rsize =  round($indexed_file_size / GB, 3);
-          echo "<td class='a'>" . $file . "</td>";
-          echo "<td class='a'>" . $indexed_file_rsize . ' GB </td>';
-          echo "<td class='a'>" . $indexed_file_mod_date . "</td>";
-          echo "</tr>";
-          continue;
-        }
-        if ( $indexed_file_size >= MB){
-          $indexed_file_rsize =  round($indexed_file_size / MB, 3);
-          echo "<td class='a'>" . $file . "</td>";
-          echo "<td class='a'>" . $indexed_file_rsize . ' MB </td>';
-          echo "<td class='a'>" . $indexed_file_mod_date . "</td>";
-          echo "</tr>";
-          continue;
-        }
-        if ( $indexed_file_size >= kB){
-          $indexed_file_rsize =  round($indexed_file_size / kB, 3);
-          echo "<td class='a'>" . $file . "</td>";
-          echo "<td class='a'>" . $indexed_file_rsize . ' kB </td>';
-          echo "<td class='a'>" . $indexed_file_mod_date . "</td>";
-          echo "</tr>";
-          continue;
-        }
-        else {
-          echo "<td class='a'>" . $file . "</td>";
-          echo "<td class='a'>" . $indexed_file_size . ' bytes </td>';
-          echo "<td class='a'>" . $indexed_file_mod_date . "</td>";
-          echo "</tr>";
-          continue;
-        }
+          $indexed_file_size = filesize($directory_served.$file);
+          $indexed_file_mod_date = date ("Y-d-m H:i:s", filemtime($directory_served.$file));
+          
+          echo "<tr class='a'>";
+          if ( $indexed_file_size >= GB){
+            $indexed_file_rsize =  round($indexed_file_size / GB, 3);
+            echo "<td class='a'>" . $file . "</td>";
+            echo "<td class='a'>" . $indexed_file_rsize . ' GB </td>';
+            echo "<td class='a'>" . $indexed_file_mod_date . "</td>";
+            echo "</tr>";
+            continue;
+          }
+          if ( $indexed_file_size >= MB){
+            $indexed_file_rsize =  round($indexed_file_size / MB, 3);
+            echo "<td class='a'>" . $file . "</td>";
+            echo "<td class='a'>" . $indexed_file_rsize . ' MB </td>';
+            echo "<td class='a'>" . $indexed_file_mod_date . "</td>";
+            echo "</tr>";
+            continue;
+          }
+          if ( $indexed_file_size >= kB){
+            $indexed_file_rsize =  round($indexed_file_size / kB, 3);
+            echo "<td class='a'>" . $file . "</td>";
+            echo "<td class='a'>" . $indexed_file_rsize . ' kB </td>';
+            echo "<td class='a'>" . $indexed_file_mod_date . "</td>";
+            echo "</tr>";
+            continue;
+          }
+          else {
+            echo "<td class='a'>" . $file . "</td>";
+            echo "<td class='a'>" . $indexed_file_size . ' bytes </td>';
+            echo "<td class='a'>" . $indexed_file_mod_date . "</td>";
+            echo "</tr>";
+            continue;
+          }
    	}
     }
     echo "</table>";
