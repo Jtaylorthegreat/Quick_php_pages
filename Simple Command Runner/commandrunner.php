b121f7e20b70163023acf38edd93efc432b6bfc3
Jtaylorthegreat <46611199+Jtaylorthegreat@users.noreply.github.com>
2024-08-28 13:52:19 -0500

    Update commandrunner.php

--- Simple Command Runner/commandrunner.php
+++ Simple Command Runner/commandrunner.php
@@ -1,9 +1,9 @@
 <?php
-echo "<head><meta http-equiv='content-type' content='text/html; charset=UTF-8'><                                                                                                                                                             title>Custom Command Runner</title></head>";
+echo "<head><meta http-equiv='content-type' content='text/html; charset=UTF-8'><title>Custom Command Runner</title></head>";
 echo "<body style=background-color:c8c9cf>";
 
 echo "<b> Input Command to run: </b> <br>";
-echo "<textarea id='actiontext' class='text' cols='50' rows='4' maxlength='250'                                                                                                                                                              name='actiontext' form='submitactionform'></textarea>";
+echo "<textarea id='actiontext' class='text' cols='50' rows='4' maxlength='250' name='actiontext' form='submitactionform'></textarea>";
 echo "<form method='post' id='submitactionform' name='submitactionform'>";
 echo "<input type='submit' name = 'subaction' value='Run Command'></form>";
 if(isset($_POST['subaction'])) {
